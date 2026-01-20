571bbc1452b04b4dbb7500f3b019973e61fedc9f
OhChangJun <cjoh0407@gmail.com>
2026-01-20 11:48:18 +0900

    2026-01-20

---
+++ shop/src/main/java/com/example/shop/dto/MemberFormDto.java
@@ -0,0 +1,17 @@
+package com.example.shop.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberFormDto {
+
+    private String name;
+    private String email;  //회원 가입이 아이디로 사용
+    private String password;
+    private String address;
+
+}
